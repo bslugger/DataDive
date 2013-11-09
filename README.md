d09850b51b7ef9e0b030cf8b638aee4e8a82a76c
akosel <a_kosel@yahoo.com>
2013-11-09 18:37:46 -0500

    Added ZIP code lines to map

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ We used this tool to convert the csv to json:
 http://www.convertcsv.com/csv-to-json.htm
 
 
+For the Cloudmade service--which the map is created with--I have an API key, but when we put the viz on the AAACF site, we may need to modify the site name or
+or create a new account for them. Probably that would be ideal.
--- js/map.js
+++ js/map.js
@@ -1,10 +1,15 @@
 $(window).bind("load",function(){
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([42.281389, -83.748333], 13);
 
-L.tileLayer('http://{s}.tile.cloudmade.com/API-key/997/256/{z}/{x}/{y}.png', {
+geojsonFeature = d3.json('data/zip.json',function(data){
+	L.geoJson(data).addTo(map);
+
+	console.log(data);
+
+L.tileLayer('http://{s}.tile.cloudmade.com/6a7ab36b926b4fc785f8a957814c8685/997/256/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
     maxZoom: 18
 }).addTo(map);
-
+});
 });
