c34ffff4f80bd830e90731674f8c0a7fc7185de5
Edgar N <edgarn@umich.edu>
2013-11-10 12:30:11 -0500

    a

--- js/map.js
+++ js/map.js
@@ -1,19 +1,90 @@
-<<<<<<< HEAD
-function loaded() {
-
-// $(window).bind("load",function(){
-
-	var map = L.map('map').setView([42.281389, -83.748333], 11);
-	var zip = 0;
-
-=======
 $(window).bind("load",function(){
 	//This centers the map on Ann Arbor, MI
 	var map = L.map('map').setView([42.281389, -83.748333], 11);
 	
 	//This loads in the geoJSON data
->>>>>>> 075cf6cb79940c0491c41dd75476d3fc4ffc3646
 	geojsonFeature = d3.json('data/zip.json',function(data){
+		//Start Edgar's Code-----------------------------------------------------------------------------
+		var jdata;
+		//getting our json data
+		$.getJSON("data/aaacfData.json", function(data){
+			jdata = data;
+		});
+		
+		function generateMap() {
+			//update map
+		}
+
+		//create a subset of data by FOI
+		var foiData = 'all';
+		var zipData;
+		var yearData = 'all';
+		var subData = [];
+		var totalAmount = 0;
+		var numOrgs;
+		var s = [];
+		var numGrants;
+		var a = [];
+
+		function foi(){
+			foiData = $(this).data('foi');
+			console.log('button clicked: ',foiData);
+			if (foiData != 'all') {
+				$('#foi6').removeClass('hidden');
+			} else {
+				$('#foi6').addClass('hidden');
+			}
+			// $('.map').remove();
+			// generateMap();
+
+			//clear any previous data inside subData and a, which act as our filtered results
+			subData = [];
+			a = [];
+			totalAmount = 0;
+			s = [];
+
+			//iterate through our full dataset to filter by FOI
+			for (var i = 0; i < jdata.length; i++) {
+				if (jdata[i].Field_aggregate === foiData) {
+					subData.push(jdata[i]);
+					ID = jdata[i].Grantee_ID;
+
+					//create the logic for inclusion in set to not count duplicates
+					if (s.indexOf(ID) > -1) {
+						//do nothing
+					} else {
+						s.push(ID);
+					}
+					var amt = jdata[i].Amount;
+					amt = parseInt(amt);
+					a.push(amt);			
+					}
+				};
+
+			// the logic to determine aggregated sums by FOI!
+			numGrants = subData.length;
+			numOrgs = s.length;
+			for (var i = 0; i < a.length; i++) {
+				totalAmount = totalAmount + a[i];
+			}
+			console.log("number of orgs", numOrgs);
+			console.log("num of grants", numGrants);
+			console.log("total awarded", totalAmount);
+		} //end foi function
+
+
+		$('.foi').on('click', foi);
+
+		
+		//the function to filter by zip code
+		function zipfilter(){
+			// console.log(zip);
+
+		}
+		zipfilter();
+		//end Edgar's Code------------------------------------------------------------------
+
+		$('.zip').on('hover', zipfilter);		
 		L.geoJson(data).addTo(map);
 	
 		//This draws the map itself at a specified position. 
@@ -34,31 +105,23 @@ $(window).bind("load",function(){
 			
 		//This is what happens on click
 		function popupContent(e){
-			var layer = e.target;
-			function generateList(){
-				var list = "";
-				
-				for (var i = 0; i < 10; i++){
-					list += '<tr><td>Organization ' + i + '</td><td>Total Dollars Awarded: $' + i + '</td> </tr>';
-				}
-				return list;
-			}
+			layer = e.target;
 			$("#orgList").html(function(){
-							return '<h1>List of Awarded Organizations in ' + layer.feature.properties.NAME 
-								+  '</h1>' + '<table>' + generateList() + '</table>';
+							console.log(jdata);
+							return '<h1>Organizations Awarded Grants<h1>';
 						})
-					      .css('display','block');
+					  .css('display','block');
 		};
 		
 		//This is what happens on mouseover
 		function highlightFeature(e){
 		var layer = e.target;
-		var year = $( "#slider" ).slider("option","value");
+		var year = 2013;
 		var numberOfRecipients = 0;
 		var totalNumberOfGrantsAwarded = 0;
 		var percentOfYearGrantMoney = 0;
 		//Here is where the tooltip message is generated. We need to put the aggregated information in here.
-		$("#blackbox").html(function(){
+		$("#blackbox").empty().html(function(){
 							return '<h1>In ' + year + ', <br>' 
 							+ layer.feature.properties.NAME + ' received: </h1>'
 							+ '<h3>Number of Recipients</h3>' + numberOfRecipients 
@@ -82,19 +145,17 @@ $(window).bind("load",function(){
 		//This is what happens after you mouseout
 		function resetHighlight(e) {
 		
-<<<<<<< HEAD
-	function popupContent(){
-		console.log("HELLO");
-		// console.log(layer.feature.properties.NAME);
-		// zipm = layer.feature.properties.NAME;
-		$('#bottomTooltipdiv').html('<span>you clicked the map</span>');
-	};
-
-	function highlightFeature(e) {
-=======
->>>>>>> 075cf6cb79940c0491c41dd75476d3fc4ffc3646
 		var layer = e.target;
-		$("#blackbox").css('display','none');
+		$("#blackbox").html(function(){
+			return '<h1>Over 60 years<br> \
+						the Ann Arbor Area received: </h1> \
+					<h3>Number of Recipients</h3> \
+					 2000 \
+					<h3>Total Number of Grants Awarded</h3> \
+					2752 \
+					<h3>Total Funds Awarded</h3> \
+					$60,000	';
+		});
 		layer.setStyle({ // highlight the feature
 			weight: 1,
 			dashArray: '',
@@ -103,106 +164,9 @@ $(window).bind("load",function(){
 			fillOpacity: 1,
 			// border-color: "white";	
 		});
-<<<<<<< HEAD
-=======
-		zip = layer.feature.properties.NAME;
-		var div = document.getElementById( 'sideTooltipZip' );
-		$('#sideTooltipZip').empty();
-		div.insertAdjacentHTML('afterBegin',layer.feature.properties.NAME);
-		
-	    layer.setStyle({ // highlight the feature
-	        weight: 5,
-	        color: '#666',
-	        dashArray: '',
-	        fillOpacity: 1
-	    });
->>>>>>> 6b2c70ed3c331a20ac0a1579fa8ad08adec62da5
-		
-	    if (!L.Browser.ie && !L.Browser.opera) {
-	        layer.bringToFront();
-	    }
-    	// map.info.update(layer.feature.properties); // Update infobox
-    	return zip;
-	};
-	function resetHighlight(e) {
-	
-    var layer = e.target;
-	
-    layer.setStyle({ // highlight the feature
-        weight: 1,
-        dashArray: '',
-		color:"white",
-		fillColor:"black",
-        fillOpacity: 1,
-		// border-color: "white";	
-    });
-	
-    if (!L.Browser.ie && !L.Browser.opera) {
-        layer.bringToFront();
-    }
-	
-    // map.info.update(layer.feature.properties); // Update infobox
-	};
-	x = 5
-	y = "red"
-	z = "yellow"
-	L.geoJson(data, {
-	onEachFeature:onEachFeature,
-    style: function(feature) {
-        switch (feature.properties.NAME) {
-            case '48105': return {color: y}
-            case '48108': return {color: "white", "weight": x}
-			default: return {color:"white",fillColor:"black",weight:1,fillOpacity:1}
-        }
-    }
-	
-	}).addTo(map);
-	
-	
-	});
-	var jdata;
-	//getting our json data
-    $.getJSON("data/aaacfData.json", function(data){
-        jdata = data;
-    });
-	
-	function generateMap() {
-		//update map
-	}
-
-	function inclusionTest(yourList,dataID){
-		//create the logic for inclusion in set to not count duplicates
-		if (yourList.indexOf(dataID) > -1) {
-			//do nothing
-		} else {
-			yourList.push(dataID);
-		}
-		return yourList;
-	}
-
-	//create a subset of data by FOI
-	var foiData = 'all';
-	var zipData;
-	var yearData = 'all';
-	var subData = [];
-	var totalAmount = 0;
-	var numOrgs;
-	var s = []; // list that will be used to check for foi inclusion
-	var numGrants;
-	var a = []; // list that will be used to calculate total amount
-
-	function foi(){
-		foiData = $(this).data('foi');
-		console.log('button clicked: ',foiData);
-		if (foiData != 'all') {
-			$('#foi6').removeClass('hidden');
-		} else {
-			$('#foi6').addClass('hidden');
-=======
 		
 		if (!L.Browser.ie && !L.Browser.opera) {
 			layer.bringToFront();
->>>>>>> 075cf6cb79940c0491c41dd75476d3fc4ffc3646
 		}
 		
 		// map.info.update(layer.feature.properties); // Update infobox
@@ -215,104 +179,8 @@ $(window).bind("load",function(){
 			switch (feature.properties.NAME) {
 				default: return {color:"white",fillColor:'rgb(45,42,43)',weight:1,fillOpacity:1}
 			}
-<<<<<<< HEAD
-=======
-		// $('.map').remove();
-		// generateMap();
-
-		//clear any previous data inside subData and a, which act as our filtered results
-		subData = [];
-		a = [];
-		totalAmount = 0;
-		s = [];
-
-		//iterate through our full dataset to filter by FOI
-		for (var i = 0; i < jdata.length; i++) {
-			if (jdata[i].Field_aggregate === foiData) {
-				subData.push(jdata[i]);
-				ID = jdata[i].Grantee_ID;
-
-				inclusionTest(s,ID);
-
-				//create the logic for inclusion in set to not count duplicates
-				// if (s.indexOf(ID) > -1) {
-				// 	//do nothing
-				// } else {
-				// 	s.push(ID);
-				// }
-				var amt = jdata[i].Amount;
-				amt = parseInt(amt);
-				a.push(amt);			
-			}
-		};
-
-		// the logic to determine aggregated sums by FOI!
-		numGrants = subData.length;
-		numOrgs = s.length;
-		for (var i = 0; i < a.length; i++) {
-			totalAmount = totalAmount + a[i];
->>>>>>> 6b2c70ed3c331a20ac0a1579fa8ad08adec62da5
-		}
-		console.log("number of orgs", numOrgs);
-		console.log("num of grants", numGrants);
-		console.log("total awarded", totalAmount);
-	} //end foi function
-
-
-	$('.foi').on('click', foi);
-
-	
-	//the function to filter by zip code
-	var zipp;
-	subDataByZip = [];
-	z = []; // list used to check for org ID inclusion by zip
-	totalAmountZip = 0;
-	y = []; // list used to hold total amount of grant money by zip
-	function zipfilter(){
-		zipp = $('#sideTooltipZip').text();
-		console.log(zipp);
-		subDataByZip = [];
-		z = [];
-		totalAmountZip = 0;
-		y = [];
-
-		//iterate through our full dataset to filter by Zip
-		for (var i = 0; i < jdata.length; i++) {
-			if (jdata[i].Zip === zipp) {
-				subDataByZip.push(jdata[i]);
-				ID = jdata[i].Grantee_ID;
-
-				inclusionTest(z, ID);
-				var amt = jdata[i].Amount;
-				amt = parseInt(amt);
-				y.push(amt);			
-			}
-		}
-
-		// the logic to determine aggregated sums by FOI!
-		numGrants = subDataByZip.length;
-		numOrgs = z.length;
-		for (var i = 0; i < y.length; i++) {
-			totalAmount = totalAmount + y[i];
-		}
-		console.log("number of orgs", numOrgs);
-		console.log("num of grants", numGrants);
-		console.log("total awarded", totalAmount);
-	}
-
-	$('#sideTooltipZip').bind('contentchanged', function() {
-	  // do something after the div content has changed
-	  zipfilter();	
-	});
-
-	$('#sideTooltipZip').trigger('contentchanged');
-	$('#map').on('mouseover', zipfilter);
-// });
-}
-$(document).ready(loaded);
-=======
 		}
 		}).addTo(map);
+
 	});
 });
->>>>>>> 075cf6cb79940c0491c41dd75476d3fc4ffc3646
